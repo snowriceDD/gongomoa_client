2907871861351b1c47e2c6877a2ac7312207b403
snowriceDD <yoons100457@gmail.com>
2023-06-15 21:13:25 +0900

    "fix: 해답을 종속성에서 찾다"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '../components/navbar';
+import { Analytics } from '@vercel/analytics/react';
  
 
 const inter = Inter({ subsets: ['latin'] })
@@ -41,6 +42,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         {children}
+        <Analytics/>
       </body>
     </html>
   )
