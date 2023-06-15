46753d0c398f974f63fe5e92f00bb94d6908301e
snowriceDD <yoons100457@gmail.com>
2023-06-15 19:19:16 +0900

    "seo:add metadata"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,40 @@
 import './globals.css'
+import { DefaultSeo } from "next-seo";
 import { Inter } from 'next/font/google'
 import Navbar from '../components/navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
+// const metadata = {
+//   title: '공고모아',
+//   description: '신입 개발자를 위한 채용 공고를 모아봅니다.',
+// }
+
 export const metadata = {
-  title: '공고모아',
+  title: '공고모아 :: 채용사이트 너무 많다',
   description: '신입 개발자를 위한 채용 공고를 모아봅니다.',
-}
+  canonical: 'https://gongomoa.vercel.app/',
+  openGraph: {
+    type: "website",
+    locale: "ko_KR",
+    url: 'https://gongomoa.vercel.app/',
+    title: '공고모아 :: 채용사이트 너무 많다',
+    site_name: "공고모아",
+    images: [
+      {
+        url: "#",
+        width: 285,
+        height: 167,
+        alt: "이미지"
+      }
+    ]
+  },
+  twitter: {
+      handle: '@handle',
+      site: '@site',
+      cardType: 'summary_large_image',
+  },
+};
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-
+      <h3>내가 찜해둔 채용공고</h3>
     </>
   )
 }
