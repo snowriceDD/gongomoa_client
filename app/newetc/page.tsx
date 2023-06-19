16bb7196deab283ae5cc9639b112b3194285ca11
snowriceDD <yoons100457@gmail.com>
2023-06-19 09:44:25 +0900

    "feat: 404 -> etc page"

---
+++ app/newetc/page.tsx
@@ -0,0 +1,10 @@
+export default async function NewETC() {
+    return (
+        <div className="list-bg">
+          <div className="list-block">
+          <h1>신입 기타 공고 모음</h1>
+          <p>update : 미구현</p>
+          </div>
+        </div>
+      )
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
             <Link href='/' className="logo">Gongomoa</Link>
             <Link href='/newfe'>프론트엔드(신입)</Link>
             <Link href='/newbe'>백엔드(신입)</Link>
-            <Link href='/list'>기타(신입)</Link>
+            <Link href='/newetc'>기타(신입)</Link>
         </div>
     );
 }
