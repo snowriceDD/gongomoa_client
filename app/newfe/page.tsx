48e9349018117f5f6ad801952e6a6a800da9d7db
snowriceDD <yoons100457@gmail.com>
2023-06-21 13:44:24 +0900

    "ui: 반응형 web"

--- app/newfe/page.tsx
+++ app/newfe/page.tsx
@@ -20,8 +20,7 @@ export default async function NewFE() {
           <div className="list-flex">
           {result.map((v:GetDB, i:number) => {
             if (v.href[1] === 'w') {
-            return (
-              <div>
+            return (        
                 <div className="list-item" key={i}>
                   <a target="_blank" href={`https://www.wanted.co.kr/${v.href}`}>
                     <p>wanted</p>
@@ -29,11 +28,9 @@ export default async function NewFE() {
                     <p>{v.title}</p>
                   </a>
                 </div>
-              </div>
             )
           } else if (v.href[1] === 'j') {
             return (
-              <div>
                 <div className="list-item" key={i}>
                   <a target="_blank" href={`https://career.programmers.co.kr${v.href}`}>
                     <p>programmers</p>
@@ -41,7 +38,6 @@ export default async function NewFE() {
                     <p>{v.title}</p>
                   </a>
                 </div>
-              </div>
             )
           }
           })}
