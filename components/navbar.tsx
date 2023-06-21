d2fa1ce4db0880f26f850ed03cc629f0a4cffa35
snowriceDD <yoons100457@gmail.com>
2023-06-21 14:09:18 +0900

    "fix: nav 수정"

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,9 +8,9 @@ export default function Navbar() {
         <div className="navbar">
             <div className="navbarWrap">
             <Link href='/' className="logo" ><Image src={Logo} alt={"Gongomoa Logo"} width={150} /></Link>
-            <Link className="link" href='/newfe'>프론트엔드(신입)</Link>
-            <Link className="link" href='/newbe'>백엔드(신입)</Link>
-            <Link className="link" href='/newetc'>기타(신입)</Link>
+            <Link className="link" href='/newfe'>프론트엔드</Link>
+            <Link className="link" href='/newbe'>백엔드</Link>
+            <Link className="link" href='/newetc'>기타</Link>
             </div>
         </div>
     );
