51700a5ed80154c99f78293743ebb5ba35629e8e
snowriceDD <yoons100457@gmail.com>
2023-06-24 12:57:34 +0900

    "update"

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="list-bg">
       <h3>기능 업데이트 중으로, DB 갱신을 잠시 중단합니다.</h3>
-      <p>update : 2023.06.21</p>
+      <p>update : 2023.06.24</p>
     </div>
   )
 }
