471d839fbb15ffa7ce60b07fc2ccbf6e78e99df3
snowriceDD <yoons100457@gmail.com>
2023-06-29 18:04:28 +0900

    "fix: textarea로 변경"

--- components/postReview.tsx
+++ components/postReview.tsx
@@ -19,10 +19,9 @@ export default function PostReview(props: PostReviewType) {
                     maxLength={8} size={10} placeholder="닉네임"></input>
                 <input className="input_pw" name="password" type="password" minLength={4}
                     maxLength={8} size={10} placeholder="비밀번호"></input>
-                <input className="input_content" name="content" type="text" minLength={10}
+                <textarea className="input_content" name="content" minLength={10}
                     maxLength={150} placeholder="내용">
-                        
-                    </input>
+                    </textarea>
                 <button className="submit" type="submit" >등록</button>
                 <button className="submitUp" type="submit" >등록 + 추천</button>
             </form>
