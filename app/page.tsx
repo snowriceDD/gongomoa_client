b3ebead1de648b65c3e1ffed218d5dbaf099a0c4
snowriceDD <yoons100457@gmail.com>
2023-08-17 20:51:52 +0900

    "Feat: add chart"

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { TwoPositionPieChart } from "@/components/chart/TwoPositionPieChart";
+
 export const metadata = {
   icons: {
     icon: './favicon.ico'
@@ -27,12 +29,12 @@ export const metadata = {
   },
 };
 
+
 export default function Home() {
   return (
     <div className="list-bg">
       <div className="list-block">
-      <h3>기업별 리뷰페이지가 생성되었습니다. 리뷰 등록 기능은 빠른 시일 내에 업데이트하겠습니다.</h3>
-      <p>update : 2023.07.14</p>
+      <TwoPositionPieChart />
       </div>
     </div>
   )
