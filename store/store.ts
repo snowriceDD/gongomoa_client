131f0be9a6b93a6965ed431e7f0eb54fad2ec94e
snowriceDD <yoons100457@gmail.com>
2023-08-26 16:44:17 +0900

    "FEAT: chart data 고민.."

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,15 @@
 import create from 'zustand';
 
-export const useStore = create((set) => {
-    let obj = {
-
-    };
-})
+export const useStore = create((set) => ({
+    data: [],
+    setData: (data: any) => set({ data }),
+    fetchData: async (url: RequestInfo | URL) => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        set({ data });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  }));
